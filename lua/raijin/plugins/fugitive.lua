4c7a12c401f64543c94ab4e44a990028335ec75a
menteLocuaz <emersondurate.45@gmail.com>
2026-05-11 08:50:43 -0500

    fix:ajuste en el fugitive

--- lua/raijin/plugins/fugitive.lua
+++ lua/raijin/plugins/fugitive.lua
@@ -1,8 +1,51 @@
 return {
 	"tpope/vim-fugitive",
-	cmd = { "G", "Git" },
+
+	cmd = {
+		"G",
+		"Git",
+	},
+
 	keys = {
-		{ "<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch" },
-		{ "<leader>gl", "Git pull<cr>", desc = "Git pull" },
+		{
+			"<leader>ga",
+			"<cmd>Git fetch --all -p<cr>",
+			desc = "Git Fetch All",
+		},
+
+		{
+			"<leader>gl",
+			"<cmd>Git pull<cr>",
+			desc = "Git Pull",
+		},
+		{
+			"<leader>gs",
+			"<cmd>Git<cr>",
+			desc = "Git Status",
+		},
+
+		{
+			"<leader>gc",
+			"<cmd>Git commit<cr>",
+			desc = "Git Commit",
+		},
+
+		{
+			"<leader>gp",
+			"<cmd>Git push<cr>",
+			desc = "Git Push",
+		},
+
+		{
+			"<leader>gd",
+			"<cmd>Gdiffsplit<cr>",
+			desc = "Git Diff",
+		},
+
+		{
+			"<leader>gb",
+			"<cmd>Git blame<cr>",
+			desc = "Git Blame",
+		},
 	},
 }
--- lua/raijin/plugins/gitsigns.lua
+++ lua/raijin/plugins/gitsigns.lua
@@ -1,7 +1,86 @@
 return {
 	"lewis6991/gitsigns.nvim",
+
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+
 	opts = {
+		signs = {
+			add = { text = "│" },
+			change = { text = "│" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+
 		numhl = true,
+		linehl = false,
+
+		word_diff = false,
+
+		current_line_blame = true,
+
+		current_line_blame_opts = {
+			delay = 300,
+			virt_text_pos = "eol",
+		},
+
 		max_file_length = 10000,
+
+		on_attach = function(bufnr)
+			local gs = require("gitsigns")
+
+			local map = function(mode, lhs, rhs, desc)
+				vim.keymap.set(mode, lhs, rhs, {
+					buffer = bufnr,
+					desc = desc,
+					silent = true,
+				})
+			end
+
+			-- Navigation
+			map("n", "]h", gs.next_hunk, "Next Hunk")
+			map("n", "[h", gs.prev_hunk, "Prev Hunk")
+
+			-- Actions
+			map("n", "<leader>hs", gs.stage_hunk, "Stage Hunk")
+			map("n", "<leader>hr", gs.reset_hunk, "Reset Hunk")
+
+			map("v", "<leader>hs", function()
+				gs.stage_hunk({
+					vim.fn.line("."),
+					vim.fn.line("v"),
+				})
+			end, "Stage Hunk")
+
+			map("v", "<leader>hr", function()
+				gs.reset_hunk({
+					vim.fn.line("."),
+					vim.fn.line("v"),
+				})
+			end, "Reset Hunk")
+
+			map("n", "<leader>hS", gs.stage_buffer, "Stage Buffer")
+			map("n", "<leader>hu", gs.undo_stage_hunk, "Undo Stage Hunk")
+			map("n", "<leader>hR", gs.reset_buffer, "Reset Buffer")
+
+			map("n", "<leader>hp", gs.preview_hunk, "Preview Hunk")
+
+			map("n", "<leader>hb", function()
+				gs.blame_line({ full = true })
+			end, "Blame Line")
+
+			map("n", "<leader>hd", gs.diffthis, "Diff This")
+
+			map("n", "<leader>hD", function()
+				gs.diffthis("~")
+			end, "Diff Against ~")
+
+			map("n", "<leader>tb", gs.toggle_current_line_blame, "Toggle Blame")
+			map("n", "<leader>td", gs.toggle_deleted, "Toggle Deleted")
+		end,
 	},
 }
