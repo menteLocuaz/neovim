dab2590c0fada54833412e36fa39d990e27c6fda
menteLocuaz <emersondurate.45@gmail.com>
2026-05-17 18:22:17 -0500

    indentaion logic

--- lua/raijin/plugins/treesitter.lua
+++ lua/raijin/plugins/treesitter.lua
@@ -1,33 +1,51 @@
+return {
+	"menteLocuaz/nvim-treesitter-cus",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local ts = require("nvim-treesitter")
 
- return {
-       "menteLocuaz/nvim-treesitter-cus",
-       event = { "BufReadPre", "BufNewFile" },
-       build = ":TSUpdate",
-       dependencies = {
-         "windwp/nvim-ts-autotag",
-       },
-       config = function()
-        local ts = require("nvim-treesitter")
-   
-        ts.setup({
-          install_dir = vim.fn.stdpath("data") .. "/site",
-        })
-   
-        local parsers = {
-          "json", "javascript", "typescript", "tsx", "yaml", "html", "css",
-          "prisma", "markdown", "markdown_inline", "svelte", "graphql",
-          "bash", "lua", "vim", "dockerfile", "gitignore", "query", "vimdoc", "c","php","go",
-        }
-        
-        ts.install(parsers)
-   
-        vim.api.nvim_create_autocmd("FileType", {
-          callback = function(args)
-            pcall(vim.treesitter.start)
-            vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-       end,
-        })
-   
-        require("nvim-ts-autotag").setup()
-      end,
-    }
+		ts.setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
+
+		local parsers = {
+			"json",
+			"javascript",
+			"typescript",
+			"tsx",
+			"yaml",
+			"html",
+			"css",
+			"prisma",
+			"markdown",
+			"markdown_inline",
+			"svelte",
+			"graphql",
+			"bash",
+			"lua",
+			"vim",
+			"dockerfile",
+			"gitignore",
+			"query",
+			"vimdoc",
+			"c",
+			"php",
+			"go",
+		}
+
+		ts.install(parsers)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				pcall(vim.treesitter.start)
+				vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+
+		require("nvim-ts-autotag").setup()
+	end,
+}
